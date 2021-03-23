fce93ca562bac029b2d0114dffe29caf23b64289
George Cope <G.Cope13@gmail.com>
2021-03-23 16:38:42 +0000

    Set up server

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const session = require('express-session');
+// require 'path' if delivering static files
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Enables use of helper functions
+const hbs = exphbs.create({helpers});
+
+const sess = {
+    secret: 'secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Uncomment if delivering static files, see line 5 
+// app.use(express.static(path.join(__dirname, '<folder_name>')));
+
+app.use(routes);
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => console.log(`Listening on port: ${PORT}. http://localhost:${PORT}`));
+});
