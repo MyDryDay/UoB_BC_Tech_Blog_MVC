f5661f80523d8cf278fa9c8a3bb8d7384bb772c6
George Cope <G.Cope13@gmail.com>
2021-06-15 15:25:04 +0100

    Added some comments

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 
+// Require files for the api & homeRoutes routes
 const apiRoutes = require('./api');
 const homeRoutes = require('homeRoutes');
 
+// Set which routes should be used for which files
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
