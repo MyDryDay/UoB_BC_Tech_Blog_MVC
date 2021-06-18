819c4568aacf034e02f51b3d88feec0a49012fd2
George Cope <G.Cope13@gmail.com>
2021-06-18 21:05:10 +0100

    Removed some unecessary options

--- models/index.js
+++ models/index.js
@@ -16,25 +16,21 @@ Post.belongsTo(User, {
 // Associations for Users and Comments
 // A User may have many Comments, a Comment only belongs to one User
 User.hasMany(Comment, {
-  foreignKey: 'user_id',
-  onDelete: "cascade"
+  foreignKey: 'user_id'
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'user_id',
-  onDelete: "cascade"
+  foreignKey: 'user_id'
 });
 
 // Associations for Posts and Comments
 // A Post may have many Comments, a Comment only belongs to one Post
 Post.hasMany(Comment, {
-  foreignKey: 'post_id',
-  onDelete: "cascade"
+  foreignKey: 'post_id'
 })
 
 Comment.belongsTo(Post, {
-  foreignKey: 'post_id',
-  onDelete: "cascade"
+  foreignKey: 'post_id'
 });
 
 module.exports = { User, Post, Comment };
