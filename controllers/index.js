6873c1b1c7afa35359ce3de0c3fd285175efca2c
George Cope <G.Cope13@gmail.com>
2021-06-18 21:46:58 +0100

    Fixed path for homeRoutes

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 // Require files for the api & homeRoutes routes
 const apiRoutes = require('./api');
-const homeRoutes = require('homeRoutes');
+const homeRoutes = require('./homeRoutes');
 
 // Set which routes should be used for which files
 router.use('/', homeRoutes);
