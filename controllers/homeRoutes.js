b51430cbf8041cb5c60d8dde1f48a8022cbc0c04
George Cope <G.Cope13@gmail.com>
2021-06-24 15:58:28 +0100

    More styling & fixed issue with comments not appearing

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -59,6 +59,8 @@ router.get('/posts/:id', async (req, res) => {
 
         const post = postData.get({ plain: true });
 
+        console.log('here');
+        console.log(post);
         res.render('post', {
             ...post,
             logged_in: req.session.logged_in
